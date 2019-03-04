7596ffa1657bb3c6dfb8fa6ca39479fb6fe37095
Meet Vaghasia <meet.bda1729@ict.gnu.ac.in>
2019-03-04 14:01:24 +0530

    Database created !

---
+++ PlacementCellP/dbo.Table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [company_name] NVARCHAR(MAX) NOT NULL, 
+    [website] NVARCHAR(50) NOT NULL, 
+    [package] FLOAT NOT NULL, 
+    [date] DATE NOT NULL, 
+    [location] NCHAR(10) NOT NULL, 
+    [eligibility] NTEXT NOT NULL
+)
